2b6cdd9a9c6830b1afb36bc3d2fedda960bcef36
Lashan Gunawardena <ravijalashangunawardena@gmail.com>
2025-11-24 19:54:29 +0530

    chore: update dependencies and refactor server setup
    - Added dotenv and updated prisma in devDependencies
    - Refactored server.js to use ES6 imports, added CORS middleware, and structured API routes
    - Created initial files for auth and user routes, and auth middleware

--- server.js
+++ server.js
@@ -1,5 +1,33 @@
-const express = require('express')
-const app = express()
-const PORT = 8383
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
 
-app.listen(PORT , () => console.log(`Server hs started on: ${PORT}`))
+//API route imports
+import authRoutes from "./routes/authRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+
+//Middleware imports
+import authMiddleware from "./middleware/authMiddleware.js";
+
+const app = express();
+const PORT = process.env.PORT || 8800;
+
+//Middleware
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json("Welcome to AMORE");
+});
+
+app.use("/auth", authRoutes);
+app.use("/users", authMiddleware, userRoutes);
+
+app.listen(PORT, () => console.log(`Server has started on ${PORT}`));
